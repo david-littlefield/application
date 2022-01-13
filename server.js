aa1e18bf8083f8d7b7b31506c402f0144c65ce4f
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2022-01-12 16:49:03 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 // imports specified modules
 const express = require("express");
-const search = require("./routes/search");
 
 // creates express application
 const server = express();
 
-// sends specified route to "search" router
-server.use("/search", search);
+// handles incoming network traffic for "root" route
+server.get("/", async function(request, response) {
+
+    // searches unsplash for "term" query
+    let urls = await app.search(request.query.term);
+    
+    // returns urls in http response
+    response.send(urls);
+
+});
 
 // listens on specified port
 server.listen(3000);
